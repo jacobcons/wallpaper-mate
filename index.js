6fe3f77f8678b81dd799f177678805326a5e9ca4
Jacob <jacobcons@gmail.com>
2024-05-13 22:22:27 +0100

    works for no interval, query provided, default resolution

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const commander_1 = __importDefault(require("commander"));
-const program = new commander_1.default.Command();
+import { Command, InvalidArgumentError } from 'commander';
+import { createWriteStream } from 'fs';
+import fetch from 'node-fetch';
+import { extname } from 'path';
+import { setWallpaper } from 'wallpaper';
+const program = new Command();
 program
-    .option('-q, --query <strings...>', "List of query terms to search for wallpapers under e.g. '-q space nature' (if not supplied => it'll search under all wallpapers)")
-    .option('-i, --interval <HH:MM:SS>', "Time interval between updating wallpaper e.g. '-i 00:30:00' (if not supplied => it'll update immediately just once)", (interval) => {
+    .option('-q, --queries <strings...>', "List of query terms to search for wallpapers under e.g. '-q space nature' (if not supplied => it'll search under all wallpapers)")
+    .option('-i, --interval <HH:MM:SS>', "Time interval between updating wallpaper e.g. '-i 00:30:00' (if not supplied => it'll update wallpaper immediately just once)", (interval) => {
     const regex = /^([01]?[0-9]|2[0-3]):([0-5]?[0-9]):([0-5]?[0-9])$/;
     const match = interval.match(regex);
     if (!match) {
-        throw new commander_1.default.InvalidArgumentError('Interval must be of format HH:mm:ss. Hours must be an integer between 0 and 23, minutes an integer between 0 and 59, and seconds an integer between 0 and 59');
+        throw new InvalidArgumentError('Interval must be of format HH:mm:ss. Hours must be an integer between 0 and 23, minutes an integer between 0 and 59, and seconds an integer between 0 and 59');
     }
-    let [_, hours, minutes, seconds] = match.map((component) => parseInt(component));
+    let [_, hours, minutes, seconds] = match.map((group) => parseInt(group));
     const MILLISECONDS_IN_SECOND = 1000;
     const MILLISECONDS_IN_MINUTE = MILLISECONDS_IN_SECOND * 60;
     const MILLISECONDS_IN_HOUR = MILLISECONDS_IN_MINUTE * 60;
@@ -22,4 +21,24 @@ program
         hours * MILLISECONDS_IN_HOUR);
 });
 program.parse();
-console.log(program.opts());
+const { queries = [''], interval } = program.opts();
+function randomChoice(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+if (!interval) {
+    const imageUrlPromises = queries.map((q) => fetch(`https://wallhaven.cc/api/v1/search?q=${q}&sorting=random&atleast=1920x1080`)
+        .then((res) => res.json())
+        .then((res) => res.data[0].path));
+    const imageUrls = await Promise.all(imageUrlPromises);
+    const imageUrl = randomChoice(imageUrls);
+    const res = await fetch(imageUrl);
+    if (!res.ok || !res.body) {
+        throw new Error('Failed to fetch image');
+    }
+    const wallPaperPath = `./wallpaper${extname(imageUrl)}`;
+    const fileStream = createWriteStream(wallPaperPath);
+    res.body.pipe(fileStream);
+    fileStream.on('finish', async () => {
+        await setWallpaper(wallPaperPath);
+    });
+}
--- index.ts
+++ index.ts
@@ -1,19 +1,23 @@
-import commander from 'commander';
-const program = new commander.Command();
+import { Command, InvalidArgumentError } from 'commander';
+import { createWriteStream } from 'fs';
+import fetch from 'node-fetch';
+import { extname } from 'path';
+import { setWallpaper } from 'wallpaper';
+const program = new Command();
 
 program
   .option(
-    '-q, --query <strings...>',
+    '-q, --queries <strings...>',
     "List of query terms to search for wallpapers under e.g. '-q space nature' (if not supplied => it'll search under all wallpapers)",
   )
   .option(
     '-i, --interval <HH:MM:SS>',
     "Time interval between updating wallpaper e.g. '-i 00:30:00' (if not supplied => it'll update wallpaper immediately just once)",
-    (interval) => {
+    (interval: string): number => {
       const regex = /^([01]?[0-9]|2[0-3]):([0-5]?[0-9]):([0-5]?[0-9])$/;
       const match = interval.match(regex);
       if (!match) {
-        throw new commander.InvalidArgumentError(
+        throw new InvalidArgumentError(
           'Interval must be of format HH:mm:ss. Hours must be an integer between 0 and 23, minutes an integer between 0 and 59, and seconds an integer between 0 and 59',
         );
       }
@@ -32,7 +36,32 @@ program
 
 program.parse();
 
-const { query, interval } = program.opts();
+const { queries = [''], interval } = program.opts();
+
+function randomChoice<T>(array: T[]): T {
+  return array[Math.floor(Math.random() * array.length)];
+}
 
 if (!interval) {
+  const imageUrlPromises = queries.map((q: string) =>
+    fetch(
+      `https://wallhaven.cc/api/v1/search?q=${q}&sorting=random&atleast=1920x1080`,
+    )
+      .then((res) => res.json())
+      .then((res: any) => res.data[0].path),
+  );
+  const imageUrls = await Promise.all(imageUrlPromises);
+  const imageUrl = randomChoice<string>(imageUrls);
+
+  const res = await fetch(imageUrl);
+  if (!res.body) {
+    throw new Error('Failed to fetch image');
+  }
+
+  const wallPaperPath = `./wallpaper${extname(imageUrl)}`;
+  const fileStream = createWriteStream(wallPaperPath);
+  res.body.pipe(fileStream);
+  fileStream.on('finish', async () => {
+    await setWallpaper(wallPaperPath);
+  });
 }
