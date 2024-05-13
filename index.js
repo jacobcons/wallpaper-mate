8f1b4794d34802587d5f51320e5f850400d32256
Jacob <jacobcons@gmail.com>
2024-05-13 19:57:44 +0100

    accept query and interval args

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 "use strict";
-function hey(times, message) {
-    console.log(message.repeat(times));
-}
-hey(4, "let's go");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const commander_1 = __importDefault(require("commander"));
+const program = new commander_1.default.Command();
+program
+    .option('-q, --query <strings...>', "List of query terms to search for wallpapers under e.g. '-q space nature' (if not supplied => it'll search under all wallpapers)")
+    .option('-i, --interval <HH:MM:SS>', "Time interval between updating wallpaper e.g. '-i 00:30:00' (if not supplied => it'll update immediately just once)", (interval) => {
+    const regex = /^([01]?[0-9]|2[0-3]):([0-5]?[0-9]):([0-5]?[0-9])$/;
+    const match = interval.match(regex);
+    if (!match) {
+        throw new commander_1.default.InvalidArgumentError('Interval must be of format HH:mm:ss. Hours must be an integer between 0 and 23, minutes an integer between 0 and 59, and seconds an integer between 0 and 59');
+    }
+    let [_, hours, minutes, seconds] = match.map((component) => parseInt(component));
+    const MILLISECONDS_IN_SECOND = 1000;
+    const MILLISECONDS_IN_MINUTE = MILLISECONDS_IN_SECOND * 60;
+    const MILLISECONDS_IN_HOUR = MILLISECONDS_IN_MINUTE * 60;
+    return (seconds * MILLISECONDS_IN_SECOND +
+        minutes * MILLISECONDS_IN_MINUTE +
+        hours * MILLISECONDS_IN_HOUR);
+});
+program.parse();
+console.log(program.opts());
--- index.ts
+++ index.ts
@@ -1,3 +1,38 @@
-function hey(times: number, message: string) : void {
-  console.log(message.repeat(times));
+import commander from 'commander';
+const program = new commander.Command();
+
+program
+  .option(
+    '-q, --query <strings...>',
+    "List of query terms to search for wallpapers under e.g. '-q space nature' (if not supplied => it'll search under all wallpapers)",
+  )
+  .option(
+    '-i, --interval <HH:MM:SS>',
+    "Time interval between updating wallpaper e.g. '-i 00:30:00' (if not supplied => it'll update wallpaper immediately just once)",
+    (interval) => {
+      const regex = /^([01]?[0-9]|2[0-3]):([0-5]?[0-9]):([0-5]?[0-9])$/;
+      const match = interval.match(regex);
+      if (!match) {
+        throw new commander.InvalidArgumentError(
+          'Interval must be of format HH:mm:ss. Hours must be an integer between 0 and 23, minutes an integer between 0 and 59, and seconds an integer between 0 and 59',
+        );
+      }
+
+      let [_, hours, minutes, seconds] = match.map((group) => parseInt(group));
+      const MILLISECONDS_IN_SECOND = 1000;
+      const MILLISECONDS_IN_MINUTE = MILLISECONDS_IN_SECOND * 60;
+      const MILLISECONDS_IN_HOUR = MILLISECONDS_IN_MINUTE * 60;
+      return (
+        seconds * MILLISECONDS_IN_SECOND +
+        minutes * MILLISECONDS_IN_MINUTE +
+        hours * MILLISECONDS_IN_HOUR
+      );
+    },
+  );
+
+program.parse();
+
+const { query, interval } = program.opts();
+
+if (!interval) {
 }
