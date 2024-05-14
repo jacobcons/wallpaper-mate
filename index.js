2e581b885200f129809ea9a5979c4069faaae295
Jacob <jacobcons@gmail.com>
2024-05-14 18:39:26 +0100

    better error handling

--- index.js
+++ index.js
@@ -4,57 +4,69 @@ import fetch from 'node-fetch';
 import { extname } from 'path';
 import { setWallpaper } from 'wallpaper';
 import { getResolution } from 'get-screen-resolution';
-const program = new Command();
-program
-    .option('-q, --queries <strings...>', "List of query terms to search for wallpapers under e.g. -q space nature (if not supplied => it'll search under all wallpapers)")
-    .option('-i, --interval <hours:minutes:seconds>', "Time interval between updating wallpaper e.g. -i 00:30:00 (if not supplied => it'll update wallpaper immediately just once)", (value) => {
-    const regex = /^(\d+):(\d+):(\d+)$/;
-    const match = value.match(regex);
-    if (!match) {
-        throw new InvalidArgumentError('Interval must be of format hours:minutes:seconds where each component is an integer e.g. 12:62:101');
+async function main() {
+    const program = new Command();
+    program
+        .option('-q, --queries <strings...>', "List of query terms to search for wallpapers under e.g. -q space nature (if not supplied => it'll search under all wallpapers)")
+        .option('-i, --interval <hours:minutes:seconds>', "Time interval between updating wallpaper e.g. -i 00:30:00 (if not supplied => it'll update wallpaper immediately just once)", (value) => {
+        const regex = /^(\d+):(\d+):(\d+)$/;
+        const match = value.match(regex);
+        if (!match) {
+            throw new InvalidArgumentError('Interval must be of format hours:minutes:seconds where each component is an integer e.g. 12:62:101');
+        }
+        let [_, hours, minutes, seconds] = match.map((group) => parseInt(group));
+        const MILLISECONDS_IN_SECOND = 1000;
+        const MILLISECONDS_IN_MINUTE = MILLISECONDS_IN_SECOND * 60;
+        const MILLISECONDS_IN_HOUR = MILLISECONDS_IN_MINUTE * 60;
+        return (seconds * MILLISECONDS_IN_SECOND +
+            minutes * MILLISECONDS_IN_MINUTE +
+            hours * MILLISECONDS_IN_HOUR);
+    })
+        .option('-r --resolution <widthxheight>', "Minimum resolution of the wallpaper e.g. -r 1920x1080 (if not supplied => fetch your screen's resolution)", (value) => {
+        const regex = /^(\d+)x(\d+)$/;
+        const match = value.match(regex);
+        if (!match) {
+            throw new InvalidArgumentError('Resolution must be of the format widthxheight where each component is an integer e.g. 1920x1080');
+        }
+        const [_, width, height] = match.map((group) => parseInt(group));
+        return { width, height };
+    });
+    program.parse();
+    const { queries = [''], interval, resolution } = program.opts();
+    let width, height;
+    try {
+        ({ width, height } = resolution || (await getResolution()));
     }
-    let [_, hours, minutes, seconds] = match.map((group) => parseInt(group));
-    const MILLISECONDS_IN_SECOND = 1000;
-    const MILLISECONDS_IN_MINUTE = MILLISECONDS_IN_SECOND * 60;
-    const MILLISECONDS_IN_HOUR = MILLISECONDS_IN_MINUTE * 60;
-    return (seconds * MILLISECONDS_IN_SECOND +
-        minutes * MILLISECONDS_IN_MINUTE +
-        hours * MILLISECONDS_IN_HOUR);
-})
-    .option('-r --resolution <widthxheight>', "Minimum resolution of the wallpaper e.g. -r 1920x1080 (if not supplied => default to your screen's resolution)", (value) => {
-    const regex = /^(\d+)x(\d+)$/;
-    const match = value.match(regex);
-    if (!match) {
-        throw new InvalidArgumentError('Resolution must be of the format widthxheight where each component is an integer e.g. 1920x1080');
+    catch (err) {
+        throw new Error("Unable to get your screen's resolution. Please supply one with the -r option e.g. -r 1920x1080");
     }
-    const [_, width, height] = match.map((group) => parseInt(group));
-    return { width, height };
-});
-program.parse();
-const { queries = [''], interval, resolution } = program.opts();
-let width, height;
+    function randomChoice(array) {
+        return array[Math.floor(Math.random() * array.length)];
+    }
+    if (!interval) {
+        const q = randomChoice(queries);
+        const wallpapersResponse = await fetch(`https://wallhaven.cc/api/v1/search?q=${q}&sorting=random&atleast=${width}x${height}`);
+        const wallpapersJson = await wallpapersResponse.json();
+        const imageUrl = wallpapersJson.data[0].path;
+        const imageResponse = await fetch(imageUrl);
+        if (!imageResponse.body) {
+            throw new Error('Failed to fetch image');
+        }
+        const wallpaperPath = `./wallpaper${extname(imageUrl)}`;
+        const fileStream = createWriteStream(wallpaperPath);
+        imageResponse.body.pipe(fileStream);
+        fileStream.on('finish', async () => {
+            await setWallpaper(wallpaperPath);
+        });
+    }
+}
+console.time();
 try {
-    ({ width, height } = resolution || (await getResolution()));
+    await main();
 }
 catch (err) {
-    throw new Error("Unable to get your screen's resolution. Please supply one with the -r option");
+    const error = err;
+    console.error(`error: ${error.message}`);
+    process.exit(1);
 }
-function randomChoice(array) {
-    return array[Math.floor(Math.random() * array.length)];
-}
-if (!interval) {
-    const q = randomChoice(queries);
-    const wallpapersResponse = await fetch(`https://wallhaven.cc/api/v1/search?q=${q}&sorting=random&atleast=${width}x${height}`);
-    const wallpapersJson = await wallpapersResponse.json();
-    const imageUrl = wallpapersJson.data[0].path;
-    const imageResponse = await fetch(imageUrl);
-    if (!imageResponse.body) {
-        throw new Error('Failed to fetch image');
-    }
-    const wallpaperPath = `./wallpaper${extname(imageUrl)}`;
-    const fileStream = createWriteStream(wallpaperPath);
-    imageResponse.body.pipe(fileStream);
-    fileStream.on('finish', async () => {
-        await setWallpaper(wallpaperPath);
-    });
-}
+console.timeEnd();
--- index.ts
+++ index.ts
@@ -5,85 +5,97 @@ import { extname } from 'path';
 import { getWallpaper, setWallpaper } from 'wallpaper';
 import { getResolution, Resolution } from 'get-screen-resolution';
 
-const program = new Command();
+async function main() {
+  const program = new Command();
 
-program
-  .option(
-    '-q, --queries <strings...>',
-    "List of query terms to search for wallpapers under e.g. -q space nature (if not supplied => it'll search under all wallpapers)",
-  )
-  .option(
-    '-i, --interval <hours:minutes:seconds>',
-    "Time interval between updating wallpaper e.g. -i 00:30:00 (if not supplied => it'll update wallpaper immediately just once)",
-    (value: string): number => {
-      const regex = /^(\d+):(\d+):(\d+)$/;
-      const match = value.match(regex);
-      if (!match) {
-        throw new InvalidArgumentError(
-          'Interval must be of format hours:minutes:seconds where each component is an integer e.g. 12:62:101',
+  program
+    .option(
+      '-q, --queries <strings...>',
+      "List of query terms to search for wallpapers under e.g. -q space nature (if not supplied => it'll search under all wallpapers)",
+    )
+    .option(
+      '-i, --interval <hours:minutes:seconds>',
+      "Time interval between updating wallpaper e.g. -i 00:30:00 (if not supplied => it'll update wallpaper immediately just once)",
+      (value: string): number => {
+        const regex = /^(\d+):(\d+):(\d+)$/;
+        const match = value.match(regex);
+        if (!match) {
+          throw new InvalidArgumentError(
+            'Interval must be of format hours:minutes:seconds where each component is an integer e.g. 12:62:101',
+          );
+        }
+
+        let [_, hours, minutes, seconds] = match.map((group) =>
+          parseInt(group),
         );
-      }
-
-      let [_, hours, minutes, seconds] = match.map((group) => parseInt(group));
-      const MILLISECONDS_IN_SECOND = 1000;
-      const MILLISECONDS_IN_MINUTE = MILLISECONDS_IN_SECOND * 60;
-      const MILLISECONDS_IN_HOUR = MILLISECONDS_IN_MINUTE * 60;
-      return (
-        seconds * MILLISECONDS_IN_SECOND +
-        minutes * MILLISECONDS_IN_MINUTE +
-        hours * MILLISECONDS_IN_HOUR
-      );
-    },
-  )
-  .option(
-    '-r --resolution <widthxheight>',
-    "Minimum resolution of the wallpaper e.g. -r 1920x1080 (if not supplied => default to your screen's resolution)",
-    (value: string): Resolution => {
-      const regex = /^(\d+)x(\d+)$/;
-      const match = value.match(regex);
-      if (!match) {
-        throw new InvalidArgumentError(
-          'Resolution must be of the format widthxheight where each component is an integer e.g. 1920x1080',
+        const MILLISECONDS_IN_SECOND = 1000;
+        const MILLISECONDS_IN_MINUTE = MILLISECONDS_IN_SECOND * 60;
+        const MILLISECONDS_IN_HOUR = MILLISECONDS_IN_MINUTE * 60;
+        return (
+          seconds * MILLISECONDS_IN_SECOND +
+          minutes * MILLISECONDS_IN_MINUTE +
+          hours * MILLISECONDS_IN_HOUR
         );
-      }
-      const [_, width, height] = match.map((group) => parseInt(group));
-      return { width, height };
-    },
-  );
+      },
+    )
+    .option(
+      '-r --resolution <widthxheight>',
+      "Minimum resolution of the wallpaper e.g. -r 1920x1080 (if not supplied => fetch your screen's resolution)",
+      (value: string): Resolution => {
+        const regex = /^(\d+)x(\d+)$/;
+        const match = value.match(regex);
+        if (!match) {
+          throw new InvalidArgumentError(
+            'Resolution must be of the format widthxheight where each component is an integer e.g. 1920x1080',
+          );
+        }
+        const [_, width, height] = match.map((group) => parseInt(group));
+        return { width, height };
+      },
+    );
 
-program.parse();
+  program.parse();
 
-const { queries = [''], interval, resolution } = program.opts();
+  const { queries = [''], interval, resolution } = program.opts();
 
-let width, height;
-try {
-  ({ width, height } = resolution || (await getResolution()));
-} catch (err) {
-  throw new Error(
-    "Unable to get your screen's resolution. Please supply one with the -r option",
-  );
-}
-
-function randomChoice<T>(array: T[]): T {
-  return array[Math.floor(Math.random() * array.length)];
-}
-
-if (!interval) {
-  const q = randomChoice<string>(queries);
-  const wallpapersResponse = await fetch(
-    `https://wallhaven.cc/api/v1/search?q=${q}&sorting=random&atleast=${width}x${height}`,
-  );
-  const wallpapersJson: any = await wallpapersResponse.json();
-  const imageUrl = wallpapersJson.data[0].path;
-
-  const imageResponse = await fetch(imageUrl);
-  if (!imageResponse.body) {
-    throw new Error('Failed to fetch image');
+  let width, height;
+  try {
+    ({ width, height } = resolution || (await getResolution()));
+  } catch (err) {
+    throw new Error(
+      "Unable to get your screen's resolution. Please supply one with the -r option e.g. -r 1920x1080",
+    );
   }
-  const wallpaperPath = `./wallpaper${extname(imageUrl)}`;
-  const fileStream = createWriteStream(wallpaperPath);
-  imageResponse.body.pipe(fileStream);
-  fileStream.on('finish', async () => {
-    await setWallpaper(wallpaperPath);
-  });
+
+  function randomChoice<T>(array: T[]): T {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  if (!interval) {
+    const q = randomChoice<string>(queries);
+    const wallpapersResponse = await fetch(
+      `https://wallhaven.cc/api/v1/search?q=${q}&sorting=random&atleast=${width}x${height}`,
+    );
+    const wallpapersJson: any = await wallpapersResponse.json();
+    const imageUrl = wallpapersJson.data[0].path;
+
+    const imageResponse = await fetch(imageUrl);
+    if (!imageResponse.body) {
+      throw new Error('Failed to fetch image');
+    }
+    const wallpaperPath = `./wallpaper${extname(imageUrl)}`;
+    const fileStream = createWriteStream(wallpaperPath);
+    imageResponse.body.pipe(fileStream);
+    fileStream.on('finish', async () => {
+      await setWallpaper(wallpaperPath);
+    });
+  }
+}
+
+try {
+  await main();
+} catch (err) {
+  const error = err as Error;
+  console.error(`error: ${error.message}`);
+  process.exit(1);
 }
