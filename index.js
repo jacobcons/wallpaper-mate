9173a9dc879f5a3d502b0ce11cc3a30a6135f02b
Jacob <jacobcons@gmail.com>
2024-05-15 12:13:52 +0100

    switched to yargs instead of commander, refactoring, examples, changed error handling, no images found error message

--- index.js
+++ index.js
@@ -1,94 +1,143 @@
-import { Command, InvalidArgumentError } from 'commander';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 import { createWriteStream, existsSync, readFileSync, writeFileSync } from 'fs';
 import fetch from 'node-fetch';
 import { extname } from 'path';
 import { setWallpaper } from 'wallpaper';
 import { getResolution } from 'get-screen-resolution';
-try {
-    await main();
-}
-catch (err) {
-    const error = err;
-    console.error(`error: ${error.message}`);
-    process.exit(1);
-}
+await main();
 async function main() {
     // setup command-line options
-    const program = new Command();
-    program
-        .option('-q, --queries <strings...>', "List of query terms to search for wallpapers under e.g. -q space nature (if not supplied => it'll search under all wallpapers)")
-        .option('-i, --interval <hours:minutes:seconds>', "Time interval between updating wallpaper e.g. -i 00:30:00 (if not supplied => it'll update wallpaper immediately just once)", (value) => {
-        // extract hours, minutes and seconds from interval and convert to total number of milliseconds
-        const regex = /^(\d+):(\d+):(\d+)$/;
-        const match = value.match(regex);
-        if (!match) {
-            throw new InvalidArgumentError('Interval must be of format hours:minutes:seconds where each component is an integer e.g. 12:62:101');
-        }
-        let [_, hours, minutes, seconds] = match.map((group) => parseInt(group));
-        const MILLISECONDS_IN_SECOND = 1000;
-        const MILLISECONDS_IN_MINUTE = MILLISECONDS_IN_SECOND * 60;
-        const MILLISECONDS_IN_HOUR = MILLISECONDS_IN_MINUTE * 60;
-        return (seconds * MILLISECONDS_IN_SECOND +
-            minutes * MILLISECONDS_IN_MINUTE +
-            hours * MILLISECONDS_IN_HOUR);
+    const y = yargs(hideBin(process.argv));
+    const { queries, interval, resolution: resolutionArgument, } = await y
+        .usage('$0 [-q <queries...>] [-i <hours> <minute> <seconds>] [-r <width> <height>]')
+        .option('queries', {
+        alias: 'q',
+        array: true,
+        description: 'List of query terms to search for wallpapers under, <queries...> e.g. space nature',
+        default: '',
+        defaultDescription: 'search under all wallpapers',
+        coerce(values) {
+            return values;
+        },
     })
-        .option('-r --resolution <widthxheight>', "Minimum resolution of the wallpaper e.g. -r 1920x1080 (if not supplied => fetch your screen's resolution)", (value) => {
-        // extract the width and height specified in resolution
-        const regex = /^(\d+)x(\d+)$/;
-        const match = value.match(regex);
-        if (!match) {
-            throw new InvalidArgumentError('Resolution must be of the format widthxheight where each component is an integer e.g. 1920x1080');
-        }
-        const [_, width, height] = match.map((group) => parseInt(group));
-        return { width, height };
-    });
-    // extract options
-    program.parse();
-    const { queries = [''], interval, resolution } = program.opts();
-    // set width and height to provided resolution option -r if given
-    // otherwise fetch the screen's resolution and cache that value to disk if needed when program is ran again
-    let width, height;
+        .option('interval', {
+        alias: 'i',
+        type: 'array',
+        description: 'Time interval between updating wallpaper, <hours> <minutes> <seconds> e.g. 0 30 0',
+        defaultDescription: 'update wallpaper immediately just once',
+        coerce(values) {
+            // ensure 3 non-negative integers are provided for hours, minutes and seconds
+            validateNonNegativeIntegerArray(values, 3, 'Interval must consist of 3 non-negative integers in the format <hours> <minutes> <seconds> e.g. 0 30 0');
+            // extract hours, minutes and seconds making up interval and compute that in total number of milliseconds
+            const [hours, minutes, seconds] = values;
+            const MILLISECONDS_IN_SECOND = 1000;
+            const MILLISECONDS_IN_MINUTE = MILLISECONDS_IN_SECOND * 60;
+            const MILLISECONDS_IN_HOUR = MILLISECONDS_IN_MINUTE * 60;
+            return (seconds * MILLISECONDS_IN_SECOND +
+                minutes * MILLISECONDS_IN_MINUTE +
+                hours * MILLISECONDS_IN_HOUR);
+        },
+    })
+        .option('resolution', {
+        alias: 'r',
+        type: 'array',
+        description: 'Minimum resolution of the wallpaper, <width> <height> e.g. 1920 1080',
+        defaultDescription: "fetch your screen's resolution",
+        coerce(values) {
+            // ensure 2 non-negative integers are provided for width and height
+            validateNonNegativeIntegerArray(values, 2, 'Resolution must consist of 2 non-negative integers in the format <width> <height> e.g. 1920 1080');
+            // extract width and height
+            const [width, height] = values;
+            return { width, height };
+        },
+    })
+        .alias('h', 'help')
+        .alias('v', 'version')
+        .example([
+        ['$0', 'Set desktop to a random wallpaper'],
+        [
+            '$0 -q space',
+            "Set desktop to a random wallpaper searching under the term 'space'",
+        ],
+        [
+            '$0 -q nature cowboy',
+            "Set desktop to a random wallpaper searching under the terms 'nature' and 'cowboy'",
+        ],
+        [
+            '$0 -q "animal crossing" -i 0 10 0',
+            "Set desktop to a random wallpaper searching under the term 'animal crossing' every 10 minutes",
+        ],
+        [
+            '$0 -i 1 0 0 -r 3840 2160 ',
+            'Set desktop to a random wallpaper every hour, only fetch wallpapers with a resolution that is at least 3840x2160',
+        ],
+    ])
+        .wrap(Math.min(160, y.terminalWidth()))
+        .parse();
+    // if resolution isn't provided as option =>
+    // fetch the screen's resolution and cache that value to disk if needed when program is ran again
+    let resolution;
     const RESOLUTION_CACHE_FILE = 'resolution.txt';
-    if (resolution) {
-        ({ width, height } = resolution);
+    if (resolutionArgument) {
+        resolution = resolutionArgument;
+    }
+    else if (existsSync(RESOLUTION_CACHE_FILE)) {
+        resolution = JSON.parse(readFileSync(RESOLUTION_CACHE_FILE, 'utf-8'));
     }
     else {
-        if (existsSync(RESOLUTION_CACHE_FILE)) {
-            const cachedResolution = readFileSync(RESOLUTION_CACHE_FILE, 'utf-8').split(' ');
-            width = cachedResolution[0];
-            height = cachedResolution[1];
+        try {
+            const fetchedResolution = await getResolution();
+            writeFileSync('resolution.txt', JSON.stringify(fetchedResolution));
+            resolution = fetchedResolution;
         }
-        else {
-            try {
-                ({ width, height } = await getResolution());
-                writeFileSync('resolution.txt', `${width} ${height}`);
-            }
-            catch (err) {
-                throw new Error("Unable to get your screen's resolution. Please supply one with the -r option e.g. -r 1920x1080");
-            }
+        catch (err) {
+            return errorAndExit("unable to get your screen's resolution. Please supply one with the -r option e.g. -r 1920x1080");
         }
-    }
-    // choose a random element from an array
-    function randomChoice(array) {
-        return array[Math.floor(Math.random() * array.length)];
     }
     // if no interval is specified => update the wallpaper just once
     if (!interval) {
-        // get the url of a wallpaper chosen from one the given query terms
-        const q = randomChoice(queries);
-        const wallpapersResponse = await fetch(`https://wallhaven.cc/api/v1/search?q=${q}&sorting=random&atleast=${width}x${height}`);
-        const wallpapersJson = await wallpapersResponse.json();
-        const imageUrl = wallpapersJson.data[0].path;
-        // get the binary data of the image at said url => save it to disk => set desktop wallpaper using it
-        const imageResponse = await fetch(imageUrl);
-        if (!imageResponse.body) {
-            throw new Error('Failed to fetch image');
-        }
-        const wallpaperPath = `./wallpaper${extname(imageUrl)}`;
-        const fileStream = createWriteStream(wallpaperPath);
-        imageResponse.body.pipe(fileStream);
-        fileStream.on('finish', async () => {
-            await setWallpaper(wallpaperPath);
-        });
+        await fetchAndSetWallpaper(queries, resolution);
     }
+    else {
+        setInterval(async () => {
+            await fetchAndSetWallpaper(queries, resolution);
+        }, interval);
+    }
+}
+// ensure array consists only of non-negative integers and is of desiredLength => if not error
+function validateNonNegativeIntegerArray(values, desiredLength, errorMsg) {
+    if (values.length !== desiredLength ||
+        values.some((value) => !Number.isInteger(value) || value < 0)) {
+        return errorAndExit(errorMsg);
+    }
+}
+function errorAndExit(msg) {
+    console.error(`error: ${msg}`);
+    process.exit(1);
+}
+function randomChoice(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+async function fetchAndSetWallpaper(queries, resolution) {
+    var _a;
+    // get the url of a wallpaper chosen from one the given query terms
+    const q = randomChoice(queries);
+    const wallpapersResponse = await fetch(`https://wallhaven.cc/api/v1/search?q=${q}&sorting=random&atleast=${resolution.width}x${resolution.height}`);
+    const wallpapersJson = await wallpapersResponse.json();
+    const imageUrl = (_a = wallpapersJson.data[0]) === null || _a === void 0 ? void 0 : _a.path;
+    if (!imageUrl) {
+        return errorAndExit('No images found with your query');
+    }
+    // get the binary data of the image at said url => save it to disk => set desktop wallpaper using it
+    const imageResponse = await fetch(imageUrl);
+    if (!imageResponse.body) {
+        return errorAndExit('Failed to fetch image');
+    }
+    const wallpaperPath = `./wallpaper${extname(imageUrl)}`;
+    const fileStream = createWriteStream(wallpaperPath);
+    imageResponse.body.pipe(fileStream);
+    fileStream.on('finish', async () => {
+        await setWallpaper(wallpaperPath);
+    });
 }
